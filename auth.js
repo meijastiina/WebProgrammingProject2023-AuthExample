3f61e5102e5b0d535e7b1bec578f6e674e174b49
“Meija <mlohiniva@hotmail.com>
2023-04-20 09:04:34 +0300

    Added auth

---
+++ auth.js
@@ -0,0 +1,7 @@
+const { sign, verify } = require("jsonwebtoken");
+// Define a secret key to sign the JWT token
+const SECRET_JWT_KEY = "PWv8VRFXgPQkfPpV4NFq";
+
+// Function to generate a JWT token
+
+// Function to verify a JWT token
