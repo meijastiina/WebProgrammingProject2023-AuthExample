eec244699207426e2813b0437c24dd3ff46175a5
“Meija <mlohiniva@hotmail.com>
2023-04-20 08:37:42 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/login", (req, res) => {
+  // This is to handle posted credentials and check if they are valid
+  res.send("Hello Login!");
+});
+
+app.get("/profile", (req, res) => {
+  // This is only accessible if the user is logged in
+  res.send("Hello Profile!");
+});
+
+app.listen(3000, () => {
+  console.log("Example app listening on port 3000!");
+});
